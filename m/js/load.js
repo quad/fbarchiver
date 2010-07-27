ef55bd35ef2355c5bd2af78db9d776e74b3f6d3f
Scott Robinson <scott@quadhome.com>
2010-07-26 18:51:26 -0700

    Added basic CSS styling on message display.

--- m/js/load.js
+++ m/js/load.js
@@ -11,34 +11,51 @@ function onMessage(event) {
 
         $('#inbox').append(function() {
             /* Render summary. */
-            r = '<li id="index-' + t.summary.threadId + '">' +
-                    '<a href="' + t.summary.icon.href + '">' +
-                        '<img class="icon" alt="' + t.summary.icon.alt + '" src="' + t.summary.icon.image + '" />' +
-                         '<span class="from">' + t.summary.icon.alt + '</span>' +
-                     '</a>' +
-                     '<span class="date">' + t.summary.timeLastUpdatedRendered + '</span>' +
-                     '<span class="subject">' + (t.summary.subject || '(no subject)') + '</span>' +
-                     '<span class="snippet">' + t.summary.snippet + '</span>';
+            r = '<tr class="meta" id="meta-' + t.summary.threadId + '">'
+                  + '<td class="face">'
+                      + '<a href="' + t.summary.icon.href + '">'
+                      +     '<img class="icon" alt="' + t.summary.icon.alt + '" src="' + t.summary.icon.image + '" />'
+                      + '</a>'
+                  + '</td>'
+                  + '<td class="who">'
+                      + '<a href="' + t.summary.icon.href + '">'
+                          + '<span class="name">' + t.summary.icon.alt + '</span>'
+                      + '</a>'
+                      + '<span class="date">' + t.summary.timeLastUpdatedRendered + '</span>'
+                  + '<td class="what">'
+                      + '<span class="subject">' + (t.summary.subject || '(no subject)') + '</span>'
+                      + '<span class="snippet">' + t.summary.snippet + '</span>'
+                  + '</td>'
+              + '</tr>';
 
             /* Render the thread. */
-            r += '<div class="thread">' +
-                    '<span class="subject">' + t.thread.subject + '</span>' +
-                    '<span class="authors">' + t.thread.renderedAuthorList + '</span>';
+            r += '<tr class="thread" id="thread-' + t.thread.threadId + '">'
+                   + '<td colspan="3">'
+                      + '<div class="header">'
+                          + '<span class="authors">' + t.thread.renderedAuthorList + '</span>'
+                      + '</div>';
 
             if (t.thread.messages) {
                 t.thread.messages.forEach(function(m) {
-                    r += '<div class="message">' +
-                            '<a href="' + m.author.href + '">' +
-                                '<img class="icon" alt="' + m.author.name + '" src="' + m.author.picture + '" />' +
-                                '<span class="from">' + m.author.name + '</span>' +
-                             '</a>' +
-                             '<span class="date">' + m.time_rendered + '</span>' +
-                             '<span class="message">' + m.message + '</span>' +
-                         '</div>';
+                    r += '<table class="message">'
+                           + '<tr>'
+                               + '<td class="face">'
+                                   + '<a href="' + m.author.href + '">'
+                                       + '<img class="icon" alt="' + m.author.name + '" src="' + m.author.picture + '" />'
+                                   + '</a>'
+                               + '</td>'
+                               + '<td class="body">'
+                                  + '<span class="from">' + m.author.name + '</span>'
+                                  + '&nbsp;'
+                                  + '<span class="date">' + m.time_rendered + '</span>'
+                                  + '<div class="message">' + m.message + '</div>'
+                               + '</td>'
+                           + '</tr>';
+                       + '</table>';
                 });
             }
 
-            r += '</div>';
+            r += '</td>' + '</tr>';
 
             return r;
         });
